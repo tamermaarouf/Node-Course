769d09ad7e054a9c051349c293c81cdd2b79e3b6
tamer_maarouf <tamermaarouf@gmail.com>
2021-10-21 04:16:28 +0200

    read from file txt hello world

--- notes-app/app.js
+++ notes-app/app.js
@@ -1,4 +1,9 @@
-const fs = require('fs');
-//fs.writeFileSync('notes.txt', 'My Name and test node.js');
+// const fs = require('fs');
+// //fs.writeFileSync('notes.txt', 'My Name and test node.js');
 
-fs.appendFileSync('notes.txt', 'test to append File');
+// fs.appendFileSync('notes.txt', 'test to append File');
+
+//import your own file
+
+const firstName = require('./utils.js');
+console.log(firstName);
---
+++ notes-app/utils.js
@@ -0,0 +1,3 @@
+console.log('utils.js');
+const name = 'tamer';
+module.exports = name;
